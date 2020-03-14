da6568b6d04e1b1716d349277b9a1e9219e66bbf
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-14 14:40:15 +0530

    ADDED:Test cases maximum float value at first,second and third position

--- src/test/java/MaximumAnalyserTest.java
+++ src/test/java/MaximumAnalyserTest.java
@@ -3,25 +3,41 @@ import org.junit.Test;
 
 public class MaximumAnalyserTest
 {
+    MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
     @Test
     public void givenThreeIntegers_FindMaximum_AtFirstPosition()
     {
-        MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
         Integer maximum = maximumAnalyser.findMaximum(98, 66, 46);
         Assert.assertEquals((Integer)98 , maximum);
     }
     @Test
     public void givenThreeIntegers_FindMaximum_AtSecondPosition()
     {
-        MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
         Integer maximum = maximumAnalyser.findMaximum(66, 98, 46);
         Assert.assertEquals((Integer)98 , maximum);
     }
     @Test
     public void givenThreeIntegers_FindMaximum_AtThirdPosition()
     {
-        MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
         Integer maximum = maximumAnalyser.findMaximum(46, 66, 98);
         Assert.assertEquals((Integer)98 , maximum);
+    }
+    @Test
+    public void givenThreeFloatNumbers_FindMaximum_AtFirstPosition()
+    {
+        Float maximum = maximumAnalyser.findMaximum(6.3f,5.6f,2.3f);
+        Assert.assertEquals((Float)6.3f , maximum);
+    }
+    @Test
+    public void givenThreeFloatNumbers_FindMaximum_AtSecondPosition()
+    {
+        Float maximum = maximumAnalyser.findMaximum(5.6f,6.3f,2.3f);
+        Assert.assertEquals((Float)6.3f , maximum);
+    }
+    @Test
+    public void givenThreeFloatNumbers_FindMaximum_AtThirdPosition()
+    {
+        Float maximum = maximumAnalyser.findMaximum(2.3f,5.6f,6.3f);
+        Assert.assertEquals((Float)6.3f , maximum);
     }
 }
