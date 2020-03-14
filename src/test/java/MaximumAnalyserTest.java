816a2537731e84b9e04e7efbd347ad9d024b2f26
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-14 15:02:51 +0530

    REFACTORED:Modified test cases according to generic method

--- src/test/java/MaximumAnalyserTest.java
+++ src/test/java/MaximumAnalyserTest.java
@@ -5,55 +5,55 @@ public class MaximumAnalyserTest
 {
     MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
     @Test
-    public void givenThreeIntegers_FindMaximum_AtFirstPosition()
+    public void givenThreeIntegers_FindMaximum_AtFirstPosition_FromGenericMethod()
     {
         Integer maximum = maximumAnalyser.findMaximum(98, 66, 46);
         Assert.assertEquals((Integer)98 , maximum);
     }
     @Test
-    public void givenThreeIntegers_FindMaximum_AtSecondPosition()
+    public void givenThreeIntegers_FindMaximum_AtSecondPosition_FromGenericMethod()
     {
         Integer maximum = maximumAnalyser.findMaximum(66, 98, 46);
         Assert.assertEquals((Integer)98 , maximum);
     }
     @Test
-    public void givenThreeIntegers_FindMaximum_AtThirdPosition()
+    public void givenThreeIntegers_FindMaximum_AtThirdPosition_FromGenericMethod()
     {
         Integer maximum = maximumAnalyser.findMaximum(46, 66, 98);
         Assert.assertEquals((Integer)98 , maximum);
     }
     @Test
-    public void givenThreeFloatNumbers_FindMaximum_AtFirstPosition()
+    public void givenThreeFloatNumbers_FindMaximum_AtFirstPosition_FromGenericMethod()
     {
         Float maximum = maximumAnalyser.findMaximum(6.3f,5.6f,2.3f);
         Assert.assertEquals((Float)6.3f , maximum);
     }
     @Test
-    public void givenThreeFloatNumbers_FindMaximum_AtSecondPosition()
+    public void givenThreeFloatNumbers_FindMaximum_AtSecondPosition_FromGenericMethod()
     {
         Float maximum = maximumAnalyser.findMaximum(5.6f,6.3f,2.3f);
         Assert.assertEquals((Float)6.3f , maximum);
     }
     @Test
-    public void givenThreeFloatNumbers_FindMaximum_AtThirdPosition()
+    public void givenThreeFloatNumbers_FindMaximum_AtThirdPosition_FromGenericMethod()
     {
         Float maximum = maximumAnalyser.findMaximum(2.3f,5.6f,6.3f);
         Assert.assertEquals((Float)6.3f , maximum);
     }
     @Test
-    public void givenThreeStrings_FindMaximum_AtFirstPosition()
+    public void givenThreeStrings_FindMaximum_AtFirstPosition_FromGenericMethod()
     {
         String maximum = maximumAnalyser.findMaximum("Zakir","Abdul","Sandip");
         Assert.assertEquals("Zakir" , maximum);
     }
     @Test
-    public void givenThreeStrings_FindMaximum_AtSecondPosition()
+    public void givenThreeStrings_FindMaximum_AtSecondPosition_FromGenericMethod()
     {
         String maximum = maximumAnalyser.findMaximum("Abdul","Zakir","Sandip");
         Assert.assertEquals("Zakir" , maximum);
     }
     @Test
-    public void givenThreeStrings_FindMaximum_AtThirdPosition()
+    public void givenThreeStrings_FindMaximum_AtThirdPosition_FromGenericMethod()
     {
         String maximum = maximumAnalyser.findMaximum("Sandip","Abdul","Zakir");
         Assert.assertEquals("Zakir" , maximum);
