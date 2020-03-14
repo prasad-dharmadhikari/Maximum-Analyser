e289b7cc59d8440fe24ed141845286b34007d2ec
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-14 14:38:04 +0530

    REFACTORED:Created method for finding maximum from three float numbers

--- src/main/java/MaximumAnalyser.java
+++ src/main/java/MaximumAnalyser.java
@@ -1,6 +1,17 @@
 public class MaximumAnalyser
 {
+    //Method for finding maximum from three integers
     public Integer findMaximum(Integer first, Integer second, Integer third)
+    {
+        if (first.compareTo(second) > 0 && first.compareTo(third) > 0)
+            return first;
+        else if (second.compareTo(first) > 0 && second.compareTo(third) > 0)
+            return second;
+        else
+            return third;
+    }
+    //Method for finding maximum from three float numbers
+    public Float findMaximum(Float first, Float second, Float third)
     {
         if (first.compareTo(second) > 0 && first.compareTo(third) > 0)
             return first;
